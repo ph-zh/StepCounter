9ca6b509cf7a387c92444eaccc5eb8714f706356
phzh <p.zhavoronkov@gmail.com>
2025-06-13 23:50:03 +0200

    printMenu added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Пришло время практики!");
+        Scanner scanner = new Scanner(System.in);
+
+        while(true) {
+            printMenu();
+
+            int command = scanner.nextInt();
+            if(command == 1) {
+                System.out.println("Frank Spignetti");
+            } else if(command == 2) {
+                System.out.println("2");
+            } else if(command == 3) {
+                System.out.println("3");
+            } else {
+                System.out.println("4");
+                scanner.close();
+                return;
+            }
+        }
+    }
+
+    public static void printMenu() {
+        System.out.println("Enter step count for a specific day");
+        System.out.println("Change daily step goal");
+        System.out.println("Print monthly statistics");
+        System.out.println("Exit application");
     }
 }
