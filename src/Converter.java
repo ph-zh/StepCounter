d1d124a1e8e65d500cad351db5416d85ec2c6f23
phzh <p.zhavoronkov@gmail.com>
2025-06-15 23:38:17 +0200

    menu added

--- src/Converter.java
+++ src/Converter.java
@@ -1,3 +1,10 @@
 public class Converter {
 
+    int convertToKm(int steps) {
+        return 0;
+    }
+
+    int convertStepsToKilocalories(int steps) {
+        return 0;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -9,23 +9,25 @@ public class Main {
 
             int command = scanner.nextInt();
             if(command == 1) {
-                System.out.println("Frank Spignetti");
+                System.out.println("Running command 1...");
             } else if(command == 2) {
-                System.out.println("2");
+                System.out.println("Running command 2...");
             } else if(command == 3) {
-                System.out.println("3");
-            } else {
-                System.out.println("4");
+                System.out.println("Running command 3...");
+            } else if (command == 0){
+                System.out.println("Exit");
                 scanner.close();
                 return;
+            } else {
+                System.out.println("Sorry, this command is not available yet.");
             }
         }
     }
 
     public static void printMenu() {
-        System.out.println("Enter step count for a specific day");
-        System.out.println("Change daily step goal");
-        System.out.println("Print monthly statistics");
-        System.out.println("Exit application");
+        System.out.println("1 - Enter step count for a specific day");
+        System.out.println("2 - Change daily step goal");
+        System.out.println("3 - Print monthly statistics");
+        System.out.println("0 - Exit application");
     }
 }
